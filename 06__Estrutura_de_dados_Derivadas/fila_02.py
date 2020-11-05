ada922d124d953f7640a36c912340263fedc0925
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-11-05 11:30:57 -0300

    fila de pessoas

---
+++ 06__Estrutura_de_dados_Derivadas/fila_02.py
@@ -0,0 +1,23 @@
+""" fila de itens """
+
+
+def fila():
+    fila = ['Daniel', 'Alessandro', 'Miguel']
+    fila.append('Bia')  # append adiciona elemento acima da fila
+    fila.append('Zen')
+    fila.append('Lion')
+    fila.append('Luke')
+    fila.append('Pequeno')
+
+    print(f'Atualmente existem essas pessas: {fila} na fila')
+    print()
+    fila.pop(0)  # pop zero remove o primeiro nome da fila
+    print(fila)
+    fila.pop(0)
+    print(fila)
+    fila.pop(0)
+    print(fila)
+    print('foram removidos os primeiros 3 nomes da fila')
+
+
+fila()
